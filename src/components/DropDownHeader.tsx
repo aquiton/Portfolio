5c4b8f83bad6fcf13cae5185562e263d5d02ff56
Alex Quiton <80876135+aquiton@users.noreply.github.com>
2025-01-07 21:38:20 -0500

    added routing and refactored components

---
+++ src/components/DropDownHeader.tsx
@@ -0,0 +1,17 @@
+
+export const DropDownHeader = () => {
+
+    return(
+        <div className="bg-green-500 flex justify-between">
+            <div className="p-2 border rounded-full font-semibold m-2">
+                <a>AQUITON</a>
+            </div>
+            <div>
+                hi
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                </svg>
+            </div>
+        </div>
+    )
+}
